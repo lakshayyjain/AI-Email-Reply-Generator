5fd688568196d1b1bdaf29f4422975684e3e092a
Lakshay Jain <130989106+lakshayyjain@users.noreply.github.com>
2025-07-11 18:19:28 +0530

    Implemented the Frontend and Backend Code for AI generated Email reply

---
+++ src/main/java/com/email/writer/EmailWriterAiApplication.java
@@ -0,0 +1,15 @@
+package com.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class EmailWriterAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterAiApplication.class, args);
+	}
+
+}
